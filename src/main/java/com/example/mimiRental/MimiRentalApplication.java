d2eb382dd5349069758388035c433e2780636e73
jahee24 <8dhfflavlr8@naver.com>
2024-12-18 14:35:28 +0900

    project name change2

--- src/main/java/com/example/mimiRental/MimiRentalApplication.java
+++ src/main/java/com/example/mimiRental/MimiRentalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MimiApplication {
+public class MimiRentalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MimiApplication.class, args);
+        SpringApplication.run(MimiRentalApplication.class, args);
     }
 
 }
